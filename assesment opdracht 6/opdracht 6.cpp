4df30e3207b618dec3d836f8514a592575c0adb5
Thijn Berkhoff <s1198746@student.windesheim.nl>
2023-10-18 09:03:25 +0200

    onvolledig assesment opdracht 6

---
+++ assesment opdracht 6/opdracht 6.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//int main()
+//{
+//	int a[10][10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//		for (int j = 0; j < 10; j++)
+//		{
+//			printf("%d", a[i][j]);
+//		}
+//}
+int main()
+{
+	int b = 0;
+	int a[10][10] = {0};
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+		{
+			//int b = 0;
+			int c = i * j;
+			printf("%d\n", a[i][j] = {c});
+		}
+			
+
+}
